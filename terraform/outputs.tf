f811e7827d9655d1ef14528365f3d184191213c3
Anton S <spaxi4@yandex.ru>
2023-08-14 18:51:16 +0300

    deploy

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -6,11 +6,6 @@ output "root_database_url" {
   value = data.template_file.environment.vars.root_database_url
 }
 
-locals {
-  api_domain        = var.api_domain[terraform.workspace]
-  web_domain        = var.web_domain[terraform.workspace]
-}
-
 output "api_origin" {
   value = "https://${local.api_domain}"
 }
