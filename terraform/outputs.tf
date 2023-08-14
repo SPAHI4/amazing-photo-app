034ef94fc2e18dabbb2054fbaa69cfcd9781f88e
Anton S <spaxi4@yandex.ru>
2023-08-14 20:00:35 +0300

    build

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,10 +1,10 @@
-output "database_url" {
-  value = data.template_file.environment.vars.database_url
-}
-
-output "root_database_url" {
-  value = data.template_file.environment.vars.root_database_url
-}
+#output "database_url" {
+#  value = data.template_file.environment.vars.database_url
+#}
+#
+#output "root_database_url" {
+#  value = data.template_file.environment.vars.root_database_url
+#}
 
 output "api_origin" {
   value = "https://${local.api_domain}"
