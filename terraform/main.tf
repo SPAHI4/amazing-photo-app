d9723c44fbbd19e70c81ab4f962231a4d41f320c
Anton S <spaxi4@yandex.ru>
2023-08-15 23:59:13 +0300

    db

--- terraform/main.tf
+++ terraform/main.tf
@@ -132,13 +132,32 @@ resource "aws_instance" "app" {
 
   key_name = aws_key_pair.deployer.key_name
 
-  vpc_security_group_ids = [aws_security_group.allow_ec2.id]
+  vpc_security_group_ids = [aws_security_group.allow_ec2.id, aws_security_group.allow_rds.id]
 
   tags = {
     Name = "photo-app"
   }
 
   user_data = data.template_file.init.rendered
+}
+
+resource "aws_security_group" "allow_rds" {
+  name        = "allow_rds"
+  description = "Allow inbound traffic from EC2 instances"
+
+  ingress {
+    description = "PostgreSQL from VPC"
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_eip" "app" {
@@ -155,14 +174,15 @@ resource "cloudflare_record" "ecs_api" {
 }
 
 resource "aws_db_instance" "default" {
-  identifier        = var.db_instance_name
-  allocated_storage = 20
-  engine            = "postgres"
-  engine_version    = "15.3"
-  instance_class    = "db.t3.micro"
-  username          = "postgres"
-  db_name           = "postgres"
-  password          = var.db_password
+  identifier             = var.db_instance_name
+  allocated_storage      = 20
+  engine                 = "postgres"
+  engine_version         = "15.3"
+  instance_class         = "db.t3.micro"
+  username               = "postgres"
+  db_name                = "postgres"
+  password               = var.db_password
+  vpc_security_group_ids = []
   tags = {
     Name = "photo-app"
   }
