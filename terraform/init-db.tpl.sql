fb2e58869ac2b653a9aa192c4e173cc6166f51e5
Anton S <spaxi4@yandex.ru>
2023-08-15 19:30:50 +0300

    db

--- terraform/init-db.tpl.sql
+++ terraform/init-db.tpl.sql
@@ -2,7 +2,7 @@ DO
 $$
   BEGIN
     IF NOT EXISTS (SELECT 1 FROM pg_roles WHERE rolname = 'app_postgraphile') THEN
-      CREATE ROLE app_postgraphile WITH LOGIN PASSWORD "${db_app_postgraphile_password}";
+      CREATE ROLE app_postgraphile WITH LOGIN PASSWORD "${db_app_password}";
     END IF;
     IF NOT EXISTS (SELECT 1 FROM pg_roles WHERE rolname = 'app_user') THEN
       CREATE ROLE app_user;
