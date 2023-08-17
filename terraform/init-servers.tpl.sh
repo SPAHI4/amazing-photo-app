ede2e966f0c92c36b4bdba92e947d2ea17b8c84e
Anton S <spaxi4@yandex.ru>
2023-08-17 19:05:05 +0300

    client

--- terraform/init-servers.tpl.sh
+++ terraform/init-servers.tpl.sh
@@ -5,20 +5,20 @@ sudo apt-get install ec2-instance-connect
 sudo apt-get install -y docker.io
 sudo systemctl start docker
 sudo systemctl enable docker
-sudo echo "${env}" > /home/ubuntu/environment.env
+echo "${env}" > /home/ubuntu/environment.env
 
-sudo docker pull ${repository_url}:server-${git_sha}
-sudo docker pull ${repository_url}:worker-${git_sha}
-sudo docker pull ${repository_url}:migrate-${git_sha}
+sudo docker pull ${repository_url}:server-"${git_sha}"
+sudo docker pull ${repository_url}:worker-"${git_sha}"
+sudo docker pull ${repository_url}:migrate-"${git_sha}"
 
 sudo docker run -d -p 443:443 --restart=always \
   --env-file /home/ubuntu/environment.env \
-  ${repository_url}:server-${git_sha}
+  "${repository_url}":server-"${git_sha}"
 
 sudo docker run -d --restart=always \
     --env-file /home/ubuntu/environment.env \
-  ${repository_url}:worker-${git_sha}
+  ${repository_url}:worker-"${git_sha}"
 
 sudo docker run -d --rm \
     --env-file /home/ubuntu/environment.env \
-  ${repository_url}:migrate-${git_sha}
+  ${repository_url}:migrate-"${git_sha}"
