2601df416d9d2f52f313b7d7f24f53df67b08c2b
Anton S <spaxi4@yandex.ru>
2023-08-17 20:02:31 +0300

    client

--- terraform/main.tf
+++ terraform/main.tf
@@ -186,18 +186,16 @@ resource "aws_iam_role_policy" "ec2" {
   name = "ec2"
   role = aws_iam_role.ec2_instance.id
 
-  policy = <<EOF
-  {
-    "Version": "2012-10-17",
-    "Statement": [
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
       {
-        "Action": "s3:*",
-        "Effect": "Allow",
-        "Resource": "*"
+        Action   = "s3:*"
+        Effect   = "Allow"
+        Resource = "*"
       }
     ]
-  }
-  EOF
+  })
 }
 
 resource "aws_instance" "app" {
