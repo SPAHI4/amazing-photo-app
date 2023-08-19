5b9effd8254093105f3aa268fb0197db17d61cfa
Anton S <spaxi4@yandex.ru>
2023-08-19 18:20:56 +0300

    convert fix

--- worker/src/tasks/convert-image.ts
+++ worker/src/tasks/convert-image.ts
@@ -112,7 +112,7 @@ const convertImage = async (
     ];
   }
 
-  const args = ['-y', '-i', source.filePath, ...commandArgs.flat(), target.filePath, '>', '/tmp/conversion.log', '2>&1'];
+  const args = ['-y', '-i', source.filePath, ...commandArgs.flat(), target.filePath];
 
   logger.debug(`🤓 Converting image: ffmpeg ${args.join(' ')}`);
 
