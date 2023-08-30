2259df166df25a27ef841514fb622e50f184999a
Anton S <spaxi4@yandex.ru>
2023-08-30 12:58:09 +0300

    Refactor environment variables and test reporter

--- jest.config.base.js
+++ jest.config.base.js
@@ -5,10 +5,16 @@ import preset from 'ts-jest/presets/index.js';
 /** @type {(dir: string) => import('ts-jest').JestConfigWithTsJest} */
 export default (dir) => {
   const packageObj = JSON.parse(fs.readFileSync(`${dir}/package.json`, 'utf8'));
+  let reporters = ['default'];
+
+  if (process.env.DEPLOYMENT === 'ci') {
+    reporters = [['github-actions', { silent: false }], 'summary'];
+  }
 
   return {
     ...preset.defaultsESM,
     testEnvironment: 'node',
+    reporters,
     testMatch: ['<rootDir>/**/__tests__/**/*.test.[t]s?(x)'],
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
     roots: [`<rootDir>`],
