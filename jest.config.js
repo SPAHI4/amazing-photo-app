8f1d7df9d5c89a6194c02d642a84419d911c9ece
Anton S <spaxi4@yandex.ru>
2023-09-05 17:14:52 +0300

    tsconfig file change

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,7 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 export default {
   projects: [
-    '<rootDir>/client/jest.config.js',
     '<rootDir>/server/jest.config.js',
-    '<rootDir>/worker/jest.config.js',
     '<rootDir>/db/jest.config.js',
     '<rootDir>/e2e/jest.config.js',
   ],
